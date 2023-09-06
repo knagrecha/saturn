1087d422c812e5fcbb0fb1f0ce3314c8e27f7a80
knagrecha <32966638+knagrecha@users.noreply.github.com>
2023-09-06 16:21:08 -0700

    Update WikiText103.py

--- examples/wikitext103/WikiText103.py
+++ examples/wikitext103/WikiText103.py
@@ -31,80 +31,76 @@ from saturn.trial_runner.PerformanceEvaluator import search
 from saturn import orchestrate
 
 
-class TestPerformanceEvaluator(unittest.TestCase):
-	def setUp(self):
+
+def setup_tasks():
+	"""
+    	Configuring hints & tokenizers for the GPT-2 & GPT-J jobs
+	"""
 		
-		"""
-            Configuring hints & tokenizers for the GPT-2 & GPT-J jobs
-        """
-		
-		gptJTokenizer = AutoTokenizer.from_pretrained(
-			"EleutherAI/gpt-j-6B")  # gpt2-xl-medium
-		if gptJTokenizer.pad_token is None:
-			gptJTokenizer.add_special_tokens({'pad_token': '[PAD]'})
-		
-		"""
-			Registering execution procedures to the library
-		"""
-		
-		self.classes = [FSDPExecutor, PipelineExecutor, SpilledExecutor]
-		self.class_names = ["FSDPExecutor",
-		                    "PipelineExecutor", "SpilledExecutor"]
+	gptJTokenizer = AutoTokenizer.from_pretrained(
+		"EleutherAI/gpt-j-6B")  # gpt2-xl-medium
+	if gptJTokenizer.pad_token is None:
+		gptJTokenizer.add_special_tokens({'pad_token': '[PAD]'})
+	
+	"""
+		Registering execution procedures to the library
+	"""
+	
+	classes = [FSDPExecutor, PipelineExecutor, SpilledExecutor]
+	class_names = ["FSDPExecutor", "PipelineExecutor", "SpilledExecutor"]
 
 
-		for c_name, c in zip(self.class_names, self.classes):
-			register(c_name, c)
-		
-		self.test_tasks = []
-		
-		"""
-			Creating the HPO sweep
-		"""
-		
-		for lr in [1e-5]:
-			self.test_tasks += [
-				Task(
-					getGPTJ,
-					partial(get_loader,
-					        16 // (x + 1),
-					        split="train",
-					        tokenizer=gptJTokenizer,
-					        tok_name='gpt-j',
-					        full_data=True),
-					pretraining_loss,
-					hparams=HParams(lr=lr, epochs=1, optimizer_cls=torch.optim.SGD),
-					hints={"is_transformer": True, "transformer_cls": {GPTJBlock, GPTJMLP}},
-				) for x in range(2)
-			]
+	for c_name, c in zip(self.class_names, self.classes):
+		register(c_name, c)
+	
+	test_tasks = []
+	
+	"""
+		Creating the HPO sweep
+	"""
+	
+	for lr in [1e-5]:
+		test_tasks += [ Task(
+							getGPTJ, # model loading function
+							partial(get_loader, # dataloading function
+								16 // (x + 1), split="train", tokenizer=gptJTokenizer, tok_name='gpt-j', full_data=True),
+							pretraining_loss, # loss function
+							hparams=HParams(lr=lr, epochs=1, optimizer_cls=torch.optim.SGD), # hparams
+							hints={"is_transformer": True, "transformer_cls": {GPTJBlock, GPTJMLP}}, # arch-specific hints
+							) for x in range(2)
+						]
+	return test_tasks
 
-	def test(self):
-		search(self.test_tasks, log=True)
-		
-		for i in self.test_tasks:
-			for gpu_count, strat in i.strategies.items():
-				print("Task: {}".format(i.name))
-				print("GPUs: {}".format(gpu_count))
-				if strat.executor is not None:
-					print("Executor: {}".format(strat.executor.name))
-				else:
-					print("Executor: {}".format(None))
-		
-		batch_2 = copy.deepcopy(self.test_tasks)
-		
-		
-		for b in batch_2:
-			b.change_name()
-			b.hparams.lr = 1e-3
-		
-		batch_3 = copy.deepcopy(self.test_tasks)
-		for b in batch_3:
-			b.change_name()
-			b.hparams.lr = 3e-3
-		
-		self.test_tasks += batch_2
-		self.test_tasks += batch_3
-		
-		orchestrate(self.test_tasks, log=True, interval=1000)
+def test(test_tasks):
+	search(test_tasks, log=True)
+	
+	for i in test_tasks:
+		for gpu_count, strat in i.strategies.items():
+			print("Task: {}".format(i.name))
+			print("GPUs: {}".format(gpu_count))
+			if strat.executor is not None:
+				print("Executor: {}".format(strat.executor.name))
+			else:
+				print("Executor: {}".format(None))
+	
+	
+	# learning rate does not affect perf, so skip re-searching on those
+	batch_2 = copy.deepcopy(test_tasks)
+	for b in batch_2:
+		b.change_name()
+		b.hparams.lr = 1e-3
+	
+	batch_3 = copy.deepcopy(test_tasks)
+	for b in batch_3:
+		b.change_name()
+		b.hparams.lr = 3e-3
+	
+	test_tasks += batch_2
+	test_tasks += batch_3
+
+	# execute
+	orchestrate(self.test_tasks, log=True, interval=1000)
 
 if __name__ == '__main__':
-	unittest.main()
+	test_tasks = setup_tasks()
+	test(test_tasks)
